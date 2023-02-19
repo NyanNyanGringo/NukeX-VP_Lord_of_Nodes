ebbcc21be5b4dba54661fdb04a5243fc78b0c9c5
NyanNyanGringo <vap-yar2@mail.ru>
2023-02-19 15:21:34 +0300

    Commit

--- HotkeyManager/menu.py
+++ HotkeyManager/menu.py
@@ -10,6 +10,7 @@ import helper_hotkey_manager as helper
 import hotkey_manager_settings as settings
 
 
+
 # Create menu and menu items with actions
 menu = nuke.menu("Nuke").addMenu(settings.tab_name_in_nuke_menu)
 
